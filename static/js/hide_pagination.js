daaeb5cb37e75b24089b02acb3118feb44ca935f
Balthasar Hofer <lebalz@outlook.com>
2021-08-03 12:05:31 +0200

    fix check of mutations

--- static/js/hide_pagination.js
+++ static/js/hide_pagination.js
@@ -1,11 +1,20 @@
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
+const isRelevantMutation = (mutation) => {
+  if ((['BODY', 'HTML'].includes(mutation.target.tagName.toUpperCase()))) {
+    return true;
+  }
+  if (mutation.target.classList.contains('menu__link')) {
+    return true;
+  }
+  return false;
+}
+
 var observer = new MutationObserver(function (mutations, observer) {
   if (mutations.length == 0) {
     return;
   }
-  const isBody = mutations.length > 1 && (['BODY', 'HTML'].includes(mutations[0].target.tagName.toUpperCase()))
-  if (!isBody && !mutations[0].target.classList.contains('menu__link')) {
+  if (!mutations.some(isRelevantMutation)) {    
     try {
       if (window._LOG_MUTATIONS_) {
         console.log(mutations);
@@ -14,6 +23,7 @@ var observer = new MutationObserver(function (mutations, observer) {
     }
     return;
   }
+
   const pagination = document.querySelector(".pagination-nav");
   if (pagination && pagination.parentElement) {
     pagination.parentElement.removeChild(pagination);
