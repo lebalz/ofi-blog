6c0ae7bded3706537048a46c78f0bcd258889a3c
Balthasar Hofer <lebalz@outlook.com>
2021-08-14 21:38:47 +0200

    udate ascii table

--- src/components/AsciiEncoding.tsx
+++ src/components/AsciiEncoding.tsx
@@ -1,46 +1,40 @@
-import * as React from 'react';
-import styles from './AsciiEncoding.module.scss';
+import * as React from "react";
+import styles from "./AsciiEncoding.module.scss";
 
-const Encoding = [...Array(16)].map((_, i) => {
-    return [...Array(16)].map((_, j) => {
-        const p = (i)*16 + j + 1
-        return {char: String.fromCharCode(p), code: p, x: i, y: j}
-    })
-})
+const arr16 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 const AsciiEncoding = () => {
   const [x, setX] = React.useState(-1);
   const [y, setY] = React.useState(-1);
 
   return (
-    <table onMouseOut={() => {
-      setX(-1);
-      setY(-1);
-    }}
+    <table
+      onMouseOut={() => {
+        setX(-1);
+        setY(-1);
+      }}
       className={styles.asciiTable}
     >
-      <tbody>
-      {
-        Encoding.map((row, i) => {
-          return (
-            <tr key={i}>
-              {
-                row.map((cell, j) => {
-                  return (<td key={j} onMouseOver={() => {
+      {arr16.map((_, i) => {
+        return (
+          <tr key={i}>
+            {arr16.map((_, j) => {
+              const p = i * 16 + j + 1;
+              return (
+                <td
+                  key={p}
+                  onMouseOver={() => {
                     setX(i);
                     setY(j);
-                  }}>
-                    {
-                      (x===i && y===j) ? cell.code : cell.char
-                    }
-                  </td>);
-                })
-              }
-            </tr>
-          )
-        })
-      }
-      </tbody>
+                  }}
+                >
+                  {x === i && y === j ? p : String.fromCharCode(p)}
+                </td>
+              );
+            })}
+          </tr>
+        );
+      })}
     </table>
   );
 };
