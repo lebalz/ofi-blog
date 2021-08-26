5dd5816cbd910a482419aaa1c24039dbca2fd83d
Balthasar Hofer <lebalz@outlook.com>
2021-08-26 12:17:29 +0200

    add ip address

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import styles from "./Answer.module.scss";
-import { getItem, setItem, _1_YEAR } from "../helpers/storage";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faTimesCircle, faQuestionCircle } from "@fortawesome/free-solid-svg-icons"
+import { getItem, setItem, _4_YEARS } from "../helpers/storage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle,
+  faTimesCircle,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import clsx from "clsx";
 
-type Types = "array" | "string";
+type Types = "array" | "string" | "text";
 const OPTIONS_REGEX = /--(?<klass>\w+)$/;
 
 interface Base {
@@ -23,15 +27,27 @@ interface ArrayProps extends Base {
   solution?: (string | number)[];
 }
 
+/**
+ * Short Text or number
+ */
 interface StringProps extends Base {
   type: "string";
-  solution: string | number;
+  solution?: string | number;
 }
 
-type Props = ArrayProps | StringProps;
+/**
+ * Long Text
+ */
+interface TextProps extends Base {
+  type: "text";
+  placeholder?: string;
+  minLength?: number;
+}
+
+type Props = ArrayProps | StringProps | TextProps;
 
 const DefaultValues = (props: Props) => {
-  if (props.type === "string") {
+  if (props.type === "string" || props.type === "text") {
     return "";
   }
   return Array(props.size).fill("");
@@ -42,11 +58,10 @@ const getDataAttr = (value: string) => {
     return value.match(OPTIONS_REGEX).groups!.klass;
   }
   return undefined;
-
-}
+};
 
 const getClassName = (value: string) => {
-  return styles[getDataAttr(value)]
+  return styles[getDataAttr(value)];
 };
 
 function sanitizeId(id: string) {
@@ -69,14 +84,14 @@ function pageId() {
 }
 
 const CheckIcon = (state) => {
-  if (state === 'correct') {
+  if (state === "correct") {
     return faCheckCircle;
   }
-  if (state === 'wrong') {
+  if (state === "wrong") {
     return faTimesCircle;
   }
   return faQuestionCircle;
-}
+};
 
 const Option = ({ value, showDataAttr }) => {
   let val = value;
@@ -84,7 +99,9 @@ const Option = ({ value, showDataAttr }) => {
   if (OPTIONS_REGEX.test(value)) {
     const data = getDataAttr(value);
     klass = styles[data];
-    val = `${value.replace(OPTIONS_REGEX, "")}${showDataAttr ? ' ' + data : ''}`;
+    val = `${value.replace(OPTIONS_REGEX, "")}${
+      showDataAttr ? " " + data : ""
+    }`;
   }
   return (
     <option value={value} className={klass}>
@@ -95,13 +112,19 @@ const Option = ({ value, showDataAttr }) => {
 
 const Answer = (props: Props) => {
   const [loaded, setLoaded] = React.useState(false);
-  const [value, setValue] = React.useState(props.type === 'array' ? [] : '');
+  const [value, setValue] = React.useState(props.type === "array" ? [] : "");
   const [contextKey, setContextKey] = React.useState("");
-  const [correctState, setCorrectState] = React.useState('unchecked');
+  const [textAreaLines,setTextAreaLines] = React.useState(2);
+  const [correctState, setCorrectState] = React.useState("unchecked");
 
   React.useEffect(() => {
     if (loaded) {
-      setItem(props.id, { value: value, type: props.type }, contextKey, _1_YEAR);
+      setItem(
+        props.id,
+        { value: value, type: props.type },
+        contextKey,
+        _4_YEARS
+      );
     }
   }, [value]);
 
@@ -111,7 +134,7 @@ const Answer = (props: Props) => {
     let saved = getItem(props.id, id, {}).value;
     const hadValue = !!saved;
     if (saved === undefined) {
-      saved = DefaultValues(props)
+      saved = DefaultValues(props);
     }
     if (props.type === "array") {
       if (Array.isArray(saved)) {
@@ -126,16 +149,28 @@ const Answer = (props: Props) => {
       }
     }
     setValue(saved);
+    checkNumberOfTextAreaLines(saved);
     setLoaded(true);
-    if (props.solution && hadValue) {
-      checkAnswer(saved)
+    if (props.type !== "text" && props.solution && hadValue) {
+      checkAnswer(saved);
     }
   }, []);
 
+  const checkNumberOfTextAreaLines = (text: string) => {
+    if (props.type !== 'text') {
+      return;
+    }
+    const lines = (text.match(/\r?\n/g) || []).length + 1;
+    if (textAreaLines !== lines) {
+      setTextAreaLines(lines);
+    }
+  }
+
   const onChange = (newVal: string, idx: number = 0) => {
-    setCorrectState('unchecked')
-    if (props.type === "string") {
+    setCorrectState("unchecked");
+    if (props.type === "string" || props.type === "text") {
       setValue(newVal);
+      checkNumberOfTextAreaLines(newVal);
     } else {
       const newArr = [
         ...(value as string[]).slice(0, idx),
@@ -147,73 +182,104 @@ const Answer = (props: Props) => {
   };
 
   const checkAnswer = (current) => {
-    setCorrectState((current) === props.solution ? 'correct' : 'wrong')
-  }
+    if (props.type === "text") {
+      return;
+    }
+    setCorrectState(current === props.solution ? "correct" : "wrong");
+  };
 
-  if (props.type === "string") {
-    return (
-      <div className={styles.answer}>
-        {props.label && <label>{props.label}</label>}
-        {props.children && <label>{props.children}</label>}
-        {props.select ? (
-          <select
-            onChange={(e) => onChange(e.target.value)}
-            value={value}
-            className={getClassName(value as string)}
-            disabled={!loaded}
-          >
-            {props.select.map((v, idx) => (
-              <Option value={v} key={idx} showDataAttr={!props.hideDataAttr} />
-            ))}
-          </select>
-        ) : (
-          <input
-            disabled={!loaded}
-            type="text"
-            onChange={(e) => onChange(e.target.value)}
-            value={value}
-          />
-        )}
-        {props.solution && (
-          <button onClick={() => checkAnswer(value)} className={clsx(styles[correctState], styles.checkButton)}>
-            <FontAwesomeIcon 
-              icon={CheckIcon(correctState)} />
-            </button>
-        )}
-      </div>
-    );
-  }
-  return (
-    <div className={styles.answer} data-loaded={loaded}>
-      {props.label && <label>{props.label}</label>}
-      {(value as string[]).map((c, i) => {
-        if (props.select) {
-          return (
+  switch (props.type) {
+    case "array":
+      return (
+        <div className={styles.answer} data-loaded={loaded}>
+          {props.label && <label>{props.label}</label>}
+          {(value as string[]).map((c, i) => {
+            if (props.select) {
+              return (
+                <select
+                  key={i}
+                  onChange={(e) => onChange(e.target.value, i)}
+                  value={c}
+                  className={getClassName(c)}
+                  disabled={!loaded}
+                >
+                  {props.select.map((v, idx) => (
+                    <Option
+                      value={v}
+                      key={idx}
+                      showDataAttr={!props.hideDataAttr}
+                    />
+                  ))}
+                </select>
+              );
+            }
+            return (
+              <input
+                key={i}
+                type="text"
+                onChange={(e) => onChange(e.target.value, i)}
+                value={c}
+                disabled={!loaded}
+              />
+            );
+          })}
+        </div>
+      );
+    case "string":
+      return (
+        <div className={styles.answer}>
+          {props.label && <label>{props.label}</label>}
+          {props.children && <label>{props.children}</label>}
+          {props.select ? (
             <select
-              key={i}
-              onChange={(e) => onChange(e.target.value, i)}
-              value={c}
-              className={getClassName(c)}
+              onChange={(e) => onChange(e.target.value)}
+              value={value}
+              className={getClassName(value as string)}
               disabled={!loaded}
             >
               {props.select.map((v, idx) => (
-                <Option value={v} key={idx} showDataAttr={!props.hideDataAttr} />
+                <Option
+                  value={v}
+                  key={idx}
+                  showDataAttr={!props.hideDataAttr}
+                />
               ))}
             </select>
-          );
-        }
-        return (
-          <input
-            key={i}
-            type="text"
-            onChange={(e) => onChange(e.target.value, i)}
-            value={c}
-            disabled={!loaded}
+          ) : (
+            <input
+              disabled={!loaded}
+              type="text"
+              onChange={(e) => onChange(e.target.value)}
+              value={value}
+            />
+          )}
+          {props.solution && (
+            <button
+              onClick={() => checkAnswer(value)}
+              className={clsx(styles[correctState], styles.checkButton)}
+            >
+              <FontAwesomeIcon icon={CheckIcon(correctState)} />
+            </button>
+          )}
+        </div>
+      );
+    case "text":
+      return (
+        <div className={styles.answer}>
+          {props.label && <label>{props.label}</label>}
+          {props.children && <label>{props.children}</label>}
+          <textarea 
+            style={{height: `${1.1*textAreaLines + 1.6}em`}} 
+            value={value} 
+            className={value.length > (props.minLength || 10) ? styles.edited : undefined}
+            disabled={!loaded} 
+            onChange={(e) => onChange(e.target.value)}
+            placeholder={props.placeholder || "Antwort"}
+            spellCheck
           />
-        );
-      })}
-    </div>
-  );
+        </div>
+      );
+  }
 };
 
 export default Answer;
