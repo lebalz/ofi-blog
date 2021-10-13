c3460b689e79af1e2e73f12570b8e56a628cecdb
Balthasar Hofer <lebalz@outlook.com>
2021-10-13 19:52:39 +0200

    disable again

--- src/stores/hooks.ts
+++ src/stores/hooks.ts
@@ -1,7 +1,5 @@
 import React from "react";
 import { rootStore, storesContext } from "./stores";
-import { enableStaticRendering } from "mobx-react-lite";
-enableStaticRendering(typeof window === "undefined");
 
 export const useStores = () => React.useContext(storesContext);
 
