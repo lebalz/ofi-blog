c294a99fa1b24b55441862008ab36ab7033345eb
Balthasar Hofer <lebalz@outlook.com>
2021-10-18 02:04:47 +0200

    remove warnings

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  presets: [
+    require.resolve('@docusaurus/core/lib/babel/preset')
+  ],
   plugins: [
     [require("@babel/plugin-proposal-decorators").default, { "legacy": true }],
     [require("@babel/plugin-proposal-class-properties").default, { "loose": false }], 
     [require("@babel/plugin-proposal-private-methods").default, { "loose": false }],
+    [require("@babel/plugin-proposal-private-property-in-object").default, { "loose": false }]
   ],
   assumptions: {
       setPublicClassFields: false
