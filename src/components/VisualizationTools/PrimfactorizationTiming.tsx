af8d70b30b3f037804e094ed79cc44c78a43c3e1
Balthasar Hofer <lebalz@outlook.com>
2021-10-21 15:56:21 +0200

    fix layout

--- src/components/VisualizationTools/PrimfactorizationTiming.tsx
+++ src/components/VisualizationTools/PrimfactorizationTiming.tsx
@@ -1,4 +1,4 @@
-import { faClipboard, faClipboardCheck, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard, faClipboardCheck, faClock, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
 import * as React from 'react';
@@ -15,24 +15,33 @@ interface StageProps {
 
 const Stage = (props: StageProps) => {
     return (
+        <>
         <div className={clsx(styles.stage)}>
-            <button className="button button--outline button--primary" onClick={props.onStage}>
+            <h4>
                 {props.label}
-            </button>
+            </h4>
             <span style={{ flexGrow: 1 }} />
-            {props.result.calculations.map((calc, idx) => {
+            <button className="button button--success button--sm" onClick={props.onStage}>
+                <FontAwesomeIcon icon={faPlay} color="white" />
+            </button>
+        </div>
+        <div className={clsx(styles.result)}>
+                        {props.result.calculations.map((calc, idx) => {
                 return (
                     <span className={clsx('badge', 'badge--secondary', styles.badge)} key={idx}>
                         {calc}
                     </span>
                 );
             })}
+            <span style={{ flexGrow: 1 }} />
             {props.result.time_ms !== undefined && (
                 <span className={clsx('badge', 'badge--success', styles.badge)}>
                     <FontAwesomeIcon icon={faClock} /> {props.result.time_ms.toFixed(1)} ms
                 </span>
             )}
+
         </div>
+        </>
     );
 };
 
@@ -41,6 +50,23 @@ const hNumber = (num: number) => {
 };
 
 const DEFAULT_RESULT = { calculations: [] };
+
+const tickFormatter = (tick: number) => {
+    if (tick < 1000) {
+        return tick;
+    }
+    const pot10 = `${tick}`.length - 1;
+    const num = `${tick}`.replace(/0*$/, '')
+    const one = num.slice(0, 1);
+    const rest = num.slice(1);
+    if (one === '1') {
+        return `10^${pot10}`
+    }
+    if (!rest) {
+        return `${num.slice(0, 1)}·10^${pot10}`
+    }
+    return `${num.slice(0, 1)}.${num.slice(1)}·10^${pot10}`
+}
 
 const PrimfactorizationTiming = () => {
     const [digits, setDigits] = React.useState(6);
@@ -238,51 +264,52 @@ const PrimfactorizationTiming = () => {
                     />
                 )}
                 {measurements.length > 0 && (
-                    <>
-                        <button
-                            className={clsx(
-                                'button',
-                                'button--outline',
-                                'button--sm',
-                                showCopied ? 'button--success' : 'button--primary',
-                                styles.copy
-                            )}
-                            onClick={onCopy}
-                        >
-                            {showCopied ? (
-                                <FontAwesomeIcon icon={faClipboardCheck} />
-                            ) : (
-                                <FontAwesomeIcon icon={faClipboard} />
-                            )}
-                        </button>
-                        <div ref={ref}>
-                            <ResponsiveContainer width="100%" height={400}>
-                                <ScatterChart
-                                    width={500}
-                                    height={300}
-                                    margin={{
-                                        top: 5,
-                                        right: 100,
-                                        bottom: 80,
-                                        left: 20,
-                                    }}
-                                >
-                                    <CartesianGrid />
-                                    <XAxis
-                                        type="number"
-                                        dataKey="product"
-                                        name="Produkt"
-                                        tickFormatter={(tick) => tick === 0 ? 0 : `${hNumber(tick)}`.slice(0, -12) + ' * 10^12'}
-                                        angle={40}
-                                        textAnchor="start"
-                                    />
-                                    <YAxis type="number" dataKey="time" name="Zeit" unit="ms" />
-                                    <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                                    <Scatter name="Mesurements" data={measurements} fill="#8884d8" />
-                                </ScatterChart>
-                            </ResponsiveContainer>
+                        <div style={{width: '100%', overflow: 'auto'}}>
+                            <button
+                                className={clsx(
+                                    'button',
+                                    'button--outline',
+                                    'button--sm',
+                                    showCopied ? 'button--success' : 'button--primary',
+                                    styles.copy
+                                )}
+                                onClick={onCopy}
+                            >
+                                {showCopied ? (
+                                    <FontAwesomeIcon icon={faClipboardCheck} />
+                                ) : (
+                                    <FontAwesomeIcon icon={faClipboard} />
+                                )}
+                            </button>
+
+                            <div ref={ref} style={{overflow: 'auto', width: 'max(100%, 500px)'}}>
+                                <ResponsiveContainer width="100%" height={400}>
+                                    <ScatterChart
+                                        width={500}
+                                        height={300}
+                                        margin={{
+                                            top: 5,
+                                            right: 100,
+                                            bottom: 80,
+                                            left: 20,
+                                        }}
+                                    >
+                                        <CartesianGrid />
+                                        <XAxis
+                                            type="number"
+                                            dataKey="product"
+                                            name="Produkt"
+                                            tickFormatter={tickFormatter}
+                                            angle={40}
+                                            textAnchor="start"
+                                        />
+                                        <YAxis type="number" dataKey="time" name="Zeit" unit="ms" />
+                                        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                                        <Scatter name="Mesurements" data={measurements} fill="#8884d8" />
+                                    </ScatterChart>
+                                </ResponsiveContainer>
+                            </div>
                         </div>
-                    </>
                 )}
             </div>
         </div>
