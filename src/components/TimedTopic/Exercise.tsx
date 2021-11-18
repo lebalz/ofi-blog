3a793cd7cb37b75f2cf35661ac75929145fa0312
Balthasar Hofer <lebalz@outlook.com>
2021-11-19 00:47:56 +0100

    fix layout

--- src/components/TimedTopic/Exercise.tsx
+++ src/components/TimedTopic/Exercise.tsx
@@ -48,6 +48,8 @@ const Exercise = observer((props: Props) => {
             <div className={clsx(styles.base)}>
                 <input
                     type="text"
+                    inputMode="numeric"
+                    pattern="[0-9]*"
                     value={ex.name}
                     onChange={action((e) => (ex.name = e.target.value))}
                     placeholder="Aufgabe"
