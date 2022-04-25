d5c1788749d530f6cbf5039f29391c1c4d95d66f
Balthasar Hofer <lebalz@outlook.com>
2022-04-25 14:04:30 +0200

    update user changer

--- src/theme/NavbarItem/index.tsx
+++ src/theme/NavbarItem/index.tsx
@@ -15,8 +15,11 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import { observer } from 'mobx-react-lite';
 import clsx from 'clsx';
 import styles from './index.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleLeft, faArrowAltCircleRight, faSync } from '@fortawesome/free-solid-svg-icons';
+import { runInAction } from 'mobx';
 
-const currentVersion = (versions: (typeof NavbarItem)[]): typeof NavbarItem | undefined => {
+const currentVersion = (versions: typeof NavbarItem[]): typeof NavbarItem | undefined => {
     return versions.find((v) => v.path.length > 1 && window.location.pathname.startsWith(v.path));
 };
 
@@ -27,28 +30,84 @@ const withLoginNavbar = (Component) => {
         if (!ExecutionEnvironment.canUseDOM || props.to !== 'login' || !userStore.current) {
             return <Component {...props} />;
         }
-        const versions = globalData['docusaurus-plugin-content-docs']['default'].versions;
+        const versions = (globalData['docusaurus-plugin-content-docs']['default'] as { versions: string[] })
+            .versions;
         const version = currentVersion(versions);
         if (userStore.current.admin) {
             return (
-                <div className={clsx(styles.navBadge, 'dropdown', 'dropdown--hoverable')}>
+                <div className={clsx('button-group')}>
                     <button
-                        className={clsx(
-                            'badge',
-                            userStore.isMyView ? 'badge--primary' : 'badge--warning'
-                        )}
+                        className={clsx('badge', 'badge--secondary', 'badge--sm', styles.badgeButton)}
+                        onClick={() => {
+                            const students = userStore.byClass(version?.name);
+                            const currentIdx = students.indexOf(userStore.currentView);
+                            runInAction(() => {
+                                if (userStore.isMyView) {
+                                    userStore.reload();
+                                } else {
+                                    userStore.setView(userStore.current);
+                                }
+                            });
+                            userStore.setView(students[currentIdx]);
+                        }}
                     >
-                        {userStore.currentView.name}
+                        <FontAwesomeIcon icon={faSync} />
                     </button>
-                    <ul className="dropdown__menu">
-                        {userStore.byClass(version?.name).map((user, idx) => (
+                    <button
+                        className={clsx('badge', 'badge--secondary', 'badge--sm', styles.badgeButton)}
+                        onClick={() => {
+                            const students = userStore.byClass(version?.name);
+                            const currentIdx = students.indexOf(userStore.currentView);
+                            if (currentIdx > 0) {
+                                userStore.setView(students[currentIdx - 1]);
+                            } else {
+                                userStore.setView(students[students.length - 1]);
+                            }
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faArrowAltCircleLeft} />
+                    </button>
+                    <div className={clsx(styles.navBadge, 'dropdown', 'dropdown--hoverable')}>
+                        <button
+                            className={clsx(
+                                'badge',
+                                userStore.isMyView ? 'badge--primary' : 'badge--warning'
+                            )}
+                            style={{ textOverflow: 'ellipsis', width: '100px', whiteSpace: 'nowrap', overflow: 'hidden' }}
+                        >
+                            {userStore.currentView.name}
+                        </button>
+                        <ul className="dropdown__menu">
+                            {userStore.byClass(version?.name).map((user, idx) => (
                                 <li key={idx} onClick={() => userStore.setView(user)}>
-                                    <div className={clsx(styles.userBadge, 'badge', 'badge--secondary', 'dropdown__link')}>
+                                    <div
+                                        className={clsx(
+                                            styles.userBadge,
+                                            'badge',
+                                            'badge--secondary',
+                                            'dropdown__link'
+                                        )}
+                                    >
                                         {user.name}
                                     </div>
                                 </li>
                             ))}
-                    </ul>
+                        </ul>
+                    </div>
+                    <button
+                        className={clsx('badge', 'badge--secondary', 'badge--sm', styles.badgeButton)}
+                        onClick={() => {
+                            const students = userStore.byClass(version?.name);
+                            const currentIdx = students.indexOf(userStore.currentView);
+                            if (currentIdx < students.length - 1) {
+                                userStore.setView(students[currentIdx + 1]);
+                            } else {
+                                userStore.setView(students[0]);
+                            }
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faArrowAltCircleRight} />
+                    </button>
                 </div>
             );
         }
