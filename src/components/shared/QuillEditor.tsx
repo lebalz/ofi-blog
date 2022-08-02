1d64f03d48d0d47659e06a0ad0a5ea9e88a7f465
Balthasar Hofer <lebalz@outlook.com>
2022-08-02 21:04:48 +0200

    use react-quilljs instead of react-quill

--- src/components/shared/QuillEditor.tsx
+++ src/components/shared/QuillEditor.tsx
@@ -4,7 +4,11 @@ import clsx from 'clsx';
 import { observer } from 'mobx-react-lite';
 import Loader from './Loader';
 
-var ReactQuill: any | undefined = undefined;
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+import { useQuill } from 'react-quilljs';
+import type { default as HeicType } from 'heic2any';
+import { resolve } from 'node:path/win32';
+import { downscaleImage } from './quill-img-compress/downscaleImage';
 
 export interface ToolbarOptions {
     bold?: boolean;
@@ -29,7 +33,7 @@ const TOOLBAR = [
     [{ header: [1, 2, 3, false] }],
     [{ color: [] }, { background: [] }], // dropdown with defaults from theme
     [{ list: 'ordered' }, { list: 'bullet' }],
-    ['image']
+    ['image'],
 ];
 
 const getToolbar = (options: ToolbarOptions) => {
@@ -97,38 +101,6 @@ const getToolbar = (options: ToolbarOptions) => {
     return toolbar;
 };
 
-const loadQuill = (callback, options: ToolbarOptions = {}) => {
-    if (ReactQuill) {
-        return callback();
-    }
-    const loadLibs = [
-        import('react-quill'),
-        import('quill'),
-        import('quill-image-compress'),
-        import('react-quill/dist/quill.snow.css'),
-    ];
-    const importPosition: { [key: string]: number } = {
-        'react-quill': 0,
-        quill: 1,
-        'react-image-compress': 2,
-    };
-    if (options.formula && !(window as any).katex) {
-        importPosition['katex'] = loadLibs.length;
-        loadLibs.push(import('katex'), import('katex/dist/katex.min.css'));
-    }
-    Promise.all(loadLibs).then((packages) => {
-        ReactQuill = packages[importPosition['react-quill']].default;
-        const Quill = packages[importPosition['quill']].default;
-        const ImageCompress = packages[importPosition['react-image-compress']].default;
-        if ('katex' in importPosition) {
-            const katex = packages[importPosition['katex']].default;
-            (window as any).katex = katex;
-        }
-        Quill.register('modules/imageCompress', ImageCompress);
-        callback();
-    });
-};
-
 export interface iTextData {
     text: React.ReactNode;
     setText: (text: React.ReactNode) => void;
@@ -148,84 +120,173 @@ interface Props {
 }
 
 const QuillEditor = observer((props: Props) => {
-    const [showQuillToolbar, setShowQuillToolbar] = React.useState(false);
-    const [quillLoaded, setQuillLoaded] = React.useState(false);
-    const quillRef = React.useRef(null);
+    const mounted = React.useRef(false);
     const { model } = props;
+    const theme = 'snow';
+    // const theme = 'bubble';
+
+    const modules = {
+        toolbar: props.toolbar
+            ? getToolbar(props.toolbar)
+            : [...TOOLBAR, ...getToolbar(props.toolbarAdd || {})],
+        // imageDrop: true
+    };
 
     const onQuillToolbarMouseDown = (e: any) => {
         e.preventDefault();
     };
+    const placeholder = props.placeholder || '✍️ Antwort...';
+
+    const formats = [
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'align',
+        'list',
+        'indent',
+        'size',
+        'header',
+        'link',
+        'image',
+        'video',
+        'color',
+        'background',
+        'clean',
+    ];
+
+    const [showQuillToolbar, setShowQuillToolbar] = React.useState(false);
+    const [heic2any, setHeic2Any] = React.useState<{ default: typeof HeicType }>(undefined);
+
+    const counterRef = React.useRef();
+    const { quill, quillRef, Quill } = useQuill({ theme, modules, formats, placeholder });
 
     React.useEffect(() => {
-        let isMounted = true;
-        loadQuill(
-            () => {
-                if (isMounted) {
-                    setQuillLoaded(true);
-                    if (quillRef && quillRef.current) {
-                        quillRef.current.editor
-                            .getModule('toolbar')
-                            .container.addEventListener('mousedown', onQuillToolbarMouseDown);
-                    }
+        if (quill) {
+            quill.on('text-change', (delta, oldDelta, source) => {
+                model.setText(quill.root.innerHTML);
+            });
+        }
+    }, [quill]);
+
+    React.useEffect(() => {
+        mounted.current = true;
+
+        const prom = import('heic2any')
+            .then((module) => {
+                if (module && mounted.current) {
+                    setHeic2Any(module);
                 }
-            },
-            { ...(props.toolbar || {}), ...(props.toolbarAdd || {}) }
-        );
+            })
+            .catch((err) => {
+                console.log('Could not load heic2any', err);
+            });
+
         return () => {
-            isMounted = false;
-            if (quillRef && quillRef.current) {
-                quillRef.current.editor
+            mounted.current = false;
+        };
+    }, []);
+
+    React.useEffect(() => {
+        if (quill) {
+            quill.clipboard.dangerouslyPasteHTML((model.text as string) || '');
+            quill.blur();
+            if (props.readonly) {
+                quill.disable();
+            }
+            quill.getModule('toolbar').addHandler('image', selectLocalImage);
+            quill.getModule('toolbar').container.addEventListener('mousedown', onQuillToolbarMouseDown);
+        }
+        return () => {
+            if (quill) {
+                quill
                     .getModule('toolbar')
                     .container.removeEventListener('mousedown', onQuillToolbarMouseDown);
             }
         };
-    }, [props]);
+    }, [quill, heic2any]);
 
-    const onChange = (newVal: string, idx: number = 0) => {
-        if (props.readonly || !model.canUpdate) {
-            return;
-        }
-        if (!quillLoaded) {
-            return;
-        }
-        model.setText(newVal);
+    // Insert Image(selected by user) to quill
+    const insertToEditor = (url) => {
+        const range = quill.getSelection();
+        quill.insertEmbed(range.index, 'image', url);
+        range.index++;
+        quill.setSelection(range, 'api');
+        // add new line
+        quill.insertText(range.index, "\n")
+        range.index++;
+        quill.setSelection(range, 'api');
     };
 
-    if (!ReactQuill || !quillLoaded || !model.loaded) {
-        return <Loader />;
+    // Open Dialog to select Image File
+    const selectLocalImage = () => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*,image/heic,image/heif');
+        input.click();
+
+        input.onchange = () => {
+            let promise: Promise<Blob>;
+            const file = input.files[0];
+            if (file.type.toLowerCase() === 'image/heic' || file.type.toLowerCase() === 'image/heif') {
+                if (heic2any) {
+                    promise = heic2any.default({
+                        blob: file,
+                        toType: 'image/jpeg',
+                        quality: 0.9,
+                    }) as Promise<Blob>;
+                }
+            } else {
+                promise = Promise.resolve(file);
+            }
+            if (promise) {
+                promise.then((img) => {
+                    const prom = new Promise(resolve => {
+                        const reader = new FileReader();
+                        reader.onload = e => {
+                            resolve(e.target.result as string);
+                        };
+                        reader.readAsDataURL(img)
+                    });
+                    prom.then((img: string) => {
+                        return downscaleImage(img, 1024, 1024, 'image/jpeg', undefined,undefined,0.5)
+                    }).then(img => {
+                        insertToEditor(img)
+                    })
+                })
+            }
+
+        };
+    };
+
+    if (Quill && !quill) {
+        // Quill.register('modules/imageDrop', ImageDrop);
+        // For execute this line only once.
+        // Quill.register('modules/counter', function(quill, options) {
+        // quill.on('text-change', function() {
+        //     const text = quill.getText();
+        //     // There are a couple issues with counting words
+        //     // this way but we'll fix these later
+        //     // counterRef.current.innerText = text.split(/\s+/).length;
+        // });
+        // });
     }
+
     return (
         <div
-            onFocus={() => !showQuillToolbar && setShowQuillToolbar(true)}
             className={clsx(styles.quillEditor)}
+            onFocus={() => !showQuillToolbar && setShowQuillToolbar(true)}
         >
-            <ReactQuill
-                ref={quillRef}
-                theme="snow"
-                readOnly={props.readonly || model.readonly}
+            <div
                 className={clsx(
                     styles.quillAnswer,
                     props.monospace && styles.monospace,
                     showQuillToolbar ? undefined : styles.disableToolbar
                 )}
-                value={model.text || ''}
-                onChange={(content, _delta, _source, _editor) => {
-                    onChange(content);
-                }}
-                modules={{
-                    toolbar: props.toolbar ? getToolbar(props.toolbar) : [...TOOLBAR, ...getToolbar(props.toolbarAdd || {})],
-                    imageCompress: {
-                        quality: 0.5, // default
-                        maxWidth: 1024, // default
-                        maxHeight: 1024, // default
-                        imageType: 'image/jpeg',
-                        debug: false, // default
-                        suppressErrorLogging: false, // default
-                    },
-                }}
-                placeholder={props.placeholder || '✍️ Antwort...'}
-            />
+            >
+                <div ref={quillRef} />
+                <div ref={counterRef} />
+            </div>
         </div>
     );
 });
