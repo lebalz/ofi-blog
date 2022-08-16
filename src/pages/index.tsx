bd1941bead7ff150e6db3048616e8df95264aa7d
Balthasar Hofer <lebalz@outlook.com>
2022-08-16 10:58:06 +0200

    add soi.ch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,58 +7,80 @@ import HomepageCourses from '../components/HomepageCourses';
 import ImageGallery from 'react-image-gallery';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary index-page', styles.heroBanner)}>
-      <div className="container index-page-title">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary index-page', styles.heroBanner)}>
+            <div className="container index-page-title">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  const images = [
-    {
-      original: './img/index/artificial-intelligence.jpg'
-    },
-    {
-      original: './img/index/robot-hand.jpg'
-    },
-    {
-      original: './img/index/security.jpg'
-    },
-    {
-      original: './img/index/network.jpg'
-    },
-    {
-      original: './img/index/computer.jpg'
-    }
-  ];
+    const images = [
+        {
+            original: './img/index/artificial-intelligence.jpg',
+        },
+        {
+            original: './img/index/robot-hand.jpg',
+        },
+        {
+            original: './img/index/security.jpg',
+        },
+        {
+            original: './img/index/network.jpg',
+        },
+        {
+            original: './img/index/computer.jpg',
+        },
+    ];
 
-  return (
-    <Layout
-      description="Informatik Website by B. Hofer">
-      <HomepageHeader />
-      <main>
-        <div className="gallery-wrapper">
-          <ImageGallery
-            items={images}
-            infinite
-            lazyLoad
-            showNav={false}
-            showThumbnails={false}
-            showFullscreenButton={false}
-            showPlayButton={false}
-            showBullets
-            slideDuration={2000}
-            slideInterval={8000}
-            autoPlay
-          />
-        </div>
-        <HomepageCourses />
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <HomepageHeader />
+            <main>
+                <div className="gallery-wrapper">
+                    <ImageGallery
+                        items={images}
+                        infinite
+                        lazyLoad
+                        showNav={false}
+                        showThumbnails={false}
+                        showFullscreenButton={false}
+                        showPlayButton={false}
+                        showBullets
+                        slideDuration={2000}
+                        slideInterval={8000}
+                        autoPlay
+                    />
+                </div>
+                <HomepageCourses />
+                <div className="card" style={{ maxWidth: 'var(--ifm-container-width)', marginRight: 'auto', marginLeft: 'auto', marginBottom: '2em', background: '#d7ff00', color: 'black' }}>
+                    <div className="card__header">
+                        <h3>🚀 Wettbewerb: Informatik-Olympiade 15.9-30.11.2022 🚀</h3>
+                    </div>
+                    <div className="card__body">
+                        <p>
+                            Programmierwettbewerb für Jugendliche. Die Erste Runde findet vom <b>16.9-30.11.2022</b>
+                            online statt.
+                        </p>
+                        <div className="card__image">
+                            <a href="https://soi.ch" target="_blank">
+                                <img
+                                    src={require('./images/soi-2022.png').default}
+                                    alt="SOI"
+                                    title="Informatik-Olympiade"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="card__footer">
+                        <a className="button button--secondary button--block" href='https://soi.ch'>Mehr Informationen auf soi.ch</a>
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    );
 }
