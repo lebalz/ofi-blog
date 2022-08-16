2d0f6bc2f056b828f44dc17bd5593e6e5f602e58
Balthasar Hofer <lebalz@outlook.com>
2022-08-16 16:40:45 +0200

    fix tracker

--- src/helpers/umami.ts
+++ src/helpers/umami.ts
@@ -1,7 +1,7 @@
 export const umamiReport = (event: string, value: Object) => {
     if (window && (window as any).umami) {
         try {
-            (window as any).umami.trackEvent(value, event);
+            (window as any).umami.trackEvent(event, value);
         } catch (e) {
             console.log('umami', e);
         }
