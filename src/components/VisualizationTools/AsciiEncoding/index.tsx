5a21f464c554686b2e2778397b61fcdc5a4366e2
Balthasar Hofer <lebalz@outlook.com>
2022-08-22 21:39:26 +0200

    fix missing body enclosure

--- src/components/VisualizationTools/AsciiEncoding/index.tsx
+++ src/components/VisualizationTools/AsciiEncoding/index.tsx
@@ -1,42 +1,44 @@
-import * as React from "react";
-import styles from "./AsciiEncoding.module.scss";
+import * as React from 'react';
+import styles from './AsciiEncoding.module.scss';
 
 const arr16 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 const AsciiEncoding = () => {
-  const [x, setX] = React.useState(-1);
-  const [y, setY] = React.useState(-1);
+    const [x, setX] = React.useState(-1);
+    const [y, setY] = React.useState(-1);
 
-  return (
-    <table
-      onMouseOut={() => {
-        setX(-1);
-        setY(-1);
-      }}
-      className={styles.asciiTable}
-    >
-      {arr16.map((_, i) => {
-        return (
-          <tr key={i}>
-            {arr16.map((_, j) => {
-              const p = i * 16 + j + 1;
-              return (
-                <td
-                  key={p}
-                  onMouseOver={() => {
-                    setX(i);
-                    setY(j);
-                  }}
-                >
-                  {x === i && y === j ? p : String.fromCharCode(p)}
-                </td>
-              );
-            })}
-          </tr>
-        );
-      })}
-    </table>
-  );
+    return (
+        <table
+            onMouseOut={() => {
+                setX(-1);
+                setY(-1);
+            }}
+            className={styles.asciiTable}
+        >
+            <tbody>
+                {arr16.map((_, i) => {
+                    return (
+                        <tr key={i}>
+                            {arr16.map((_, j) => {
+                                const p = i * 16 + j + 1;
+                                return (
+                                    <td
+                                        key={p}
+                                        onMouseOver={() => {
+                                            setX(i);
+                                            setY(j);
+                                        }}
+                                    >
+                                        {x === i && y === j ? p : String.fromCharCode(p)}
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 
 export default AsciiEncoding;
