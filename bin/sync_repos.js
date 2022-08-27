d135039889d139416a5d7c83ffae7b17f6c76a5d
Balthasar Hofer <lebalz@outlook.com>
2022-08-27 10:42:49 +0200

    add sync repo script

---
+++ bin/sync_repos.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require("path");
+const { exit } = require('process');
+const { exec } = require('child_process');
+const ROOT = '/Users/balz/Documents/git_code/bh0fer/ef-repos';
+/** @type {{email: string, name: string, repo: string, url: string}[]} */
+const REPO_LIST = require('../no-docs/ef-2024-repos.json');
+/** @type {{clone?: boolean, sync?: boolean, create_review_branch?: boolean}} */
+var argv = require('minimist')(process.argv.slice(2));
+
+
+const report = (error, stdout, stderr) => {
+    if (error) {
+        return console.error(`error: ${error.message}`);
+    }
+    if (stderr) {
+        return console.error(`stderr: ${stderr}`);
+    }
+    console.log(`stdout: ${stdout}`);
+}
+
+console.log(argv)
+REPO_LIST.forEach((repo) => {
+    const repo_dir = `${repo.email.split('.')[0].toLowerCase()}-${repo.name}`;
+    const abs_path = path.join(ROOT, repo_dir);
+    if (argv.clone) {
+        return exec(`git clone ${repo.url} ${abs_path}`, report)
+    }
+    if (argv.sync) {
+        exec(`cd ${abs_path} && git reset --hard HEAD && git pull`, report)
+    }
+    if (argv.create_review_branch) {
+        exec(`cd ${abs_path} && git branch -r`, (err, stdout, stderr) => {
+            if (!stdout.match('origin/comments')) {
+                // console.log(repo.name, stdout)
+                exec(`cd ${abs_path} && git branch comments \`git rev-list --max-parents=0 HEAD | tail -n 1\` && git push -u origin comments`, report)
+            }
+        })
+    }
+})
