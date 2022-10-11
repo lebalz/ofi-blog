d4240572a784c53aa1cb26e26e2e1935988e4a7a
Balthasar Hofer <lebalz@outlook.com>
2022-10-11 22:07:12 +0200

    add life hacks

---
+++ docs/Life-Hacks/Browser-Scrollbars/README.md
@@ -0,0 +1,47 @@
+---
+sidebar_custom_props:
+  id: f874325d-e6c3-4aec-9d95-bfc6741474c7
+---
+import BrowserWindow from '@site/src/components/BrowserWindow';
+
+# Browser Scrollbalken
+
+![Moderne Scrollbars unter Windows](images/scrollbars.png)
+
+Unter Chrome :mdi-google-chrome--blue:, Edge :mdi-microsoft-edge--cyan: und Firefox :mdi-firefox--orange: gibt es eine versteckte Einstellungsseite, in welcher jegliche Einstellungen des Browsers konfiguriert werden kann. **Achtung!** Ändern Sie nur Einstellungen deren Auswirkungen Sie kennen!
+
+
+## Scrollbars
+Die Scrollbars sehen unter Windows teils etwas altbacken aus, damit Sie auf allen Gerätekategorien und für alle Benutzer:innen gut benutzbar sind. Dies kann aber auch nervig sein, da durch die Scrollbars viel Platz verloren geht. Über die versteckte Einstellungsseite lässt sich das Aussehen ändern
+
+
+<BrowserWindow url="edge://flags" browser="edge" copy>
+
+Edge :mdi-microsoft-edge--cyan:
+: **Einstellungsseite** 👉 `edge://flags`
+Aussehen der Scrollbar
+: `edge://flags/#edge-overlay-scrollbars-win-style`
+Flüssiges Scrollen
+: `edge://flags/#smooth-scrolling`
+
+</BrowserWindow>
+
+<BrowserWindow url="chrome://flags" browser="chrome" copy>
+
+Chrome :mdi-google-chrome--blue:
+: **Einstellungsseite** 👉 `chrome://flags`
+Aussehen der Scrollbar
+: `chrome://flags/#overlay-scrollbars`
+Flüssiges Scrollen
+: `chrome://flags/#smooth-scrolling`
+
+</BrowserWindow>
+
+<BrowserWindow url="about:config" browser="firefox" copy>
+
+Firefox :mdi-firefox--orange:
+: **Einstellungsseite** 👉 `about:config`
+Scrollbars
+: Standardmässig aktiviert ✅
+
+</BrowserWindow>
--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -5,43 +5,115 @@
  * LICENSE file in the root directory of this source tree.
  */
 
- import React, {type ReactNode} from 'react';
- import clsx from 'clsx';
- 
- import styles from './styles.module.scss';
- 
- interface Props {
-   children: ReactNode;
-   minHeight: number;
-   url: string;
- }
- 
- export default function BrowserWindow({
-   children,
-   minHeight,
-   url = 'http://localhost:3000',
- }: Props): JSX.Element {
-   return (
-     <div className={styles.browserWindow} style={{minHeight}}>
-       <div className={styles.browserWindowHeader}>
-         <div className={styles.buttons}>
-           <span className={styles.dot} style={{background: '#f25f58'}} />
-           <span className={styles.dot} style={{background: '#fbbe3c'}} />
-           <span className={styles.dot} style={{background: '#58cb42'}} />
-         </div>
-         <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
-          <a href={url} target="_blank">{url}</a>
-         </div>
-         <div className={styles.browserWindowMenuIcon}>
-           <div>
-             <span className={styles.bar} />
-             <span className={styles.bar} />
-             <span className={styles.bar} />
-           </div>
-         </div>
-       </div>
- 
-       <div className={styles.browserWindowBody}>{children}</div>
-     </div>
-   );
- }
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
+import { filterProps } from 'recharts/types/util/types';
+
+type Browser = 'edge' | 'chrome' | 'firefox' | 'safari'
+interface Props {
+  children: ReactNode;
+  minHeight: number;
+  url: string;
+  browser?: Browser;
+  copy?: boolean;
+}
+
+const ICONS: { [key in Browser]: string } = {
+  chrome: 'mdi-google-chrome',
+  edge: 'mdi-microsoft-edge',
+  firefox: 'mdi-firefox',
+  safari: 'mdi-apple-safari'
+}
+
+const COLOR: { [key in Browser]: string } = {
+  chrome: 'blue',
+  edge: 'cyan',
+  firefox: 'orange',
+  safari: 'blue'
+}
+type CopyState = 'spin' | 'copied' | 'error';
+
+const CopyIcon: { [key in CopyState]: string } = {
+  copied: 'mdi-clipboard-check',
+  error: 'mdi-clipboard-text-off',
+  spin: 'mdi-loading'
+}
+
+const CopyColor: { [key in CopyState]: string } = {
+  copied: 'green',
+  error: 'red',
+  spin: 'black'
+}
+
+export default function BrowserWindow({
+  children,
+  minHeight,
+  copy,
+  url = 'http://localhost:3000',
+  browser
+}: Props): JSX.Element {
+  const [copyState, setCopyState] = React.useState<CopyState | 'none'>('none');
+
+  React.useEffect(() => {
+    if (['none', 'spin', 'ready'].includes(copyState)) {
+      return;
+    }
+    const timeoutId = setTimeout(() => setCopyState('none'), 2000);
+    return () => clearTimeout(timeoutId);
+  }, [copyState]);
+
+  const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (!copy) {
+      return;
+    }
+    e.preventDefault();
+    e.stopPropagation();
+    setCopyState('spin');
+    try {
+      navigator.clipboard.writeText(url).then(() => {
+        setCopyState('copied');
+      })
+        .catch((err) => {
+          setCopyState('error');
+          console.warn(err);
+        });
+    } catch (err) {
+      console.warn(err);
+      setCopyState('error');
+    }
+  }
+  return (
+    <div className={styles.browserWindow} style={{ minHeight }}>
+      <div className={styles.browserWindowHeader}>
+        <div className={styles.buttons}>
+          <span className={styles.dot} style={{ background: '#f25f58' }} />
+          <span className={styles.dot} style={{ background: '#fbbe3c' }} />
+          <span className={styles.dot} style={{ background: '#58cb42' }} />
+        </div>
+        <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
+          {browser && (
+            <i className={clsx('mdi', ICONS[browser], COLOR[browser], styles.browserType)} />
+          )}
+          <a href={url} target="_blank" onClick={onClick}>{url}</a>
+          {copyState !== 'none' && (
+            <>
+              <div className={styles.spacer} />
+              <i className={clsx('mdi', CopyIcon[copyState], CopyColor[copyState], styles.copyState)} />
+            </>
+          )}
+        </div>
+        <div className={styles.browserWindowMenuIcon}>
+          <div>
+            <span className={styles.bar} />
+            <span className={styles.bar} />
+            <span className={styles.bar} />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.browserWindowBody}>{children}</div>
+    </div>
+  );
+}
