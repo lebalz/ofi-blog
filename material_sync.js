88aa3bf5956e284733d363fcc9f01fc3313c95f7
Balthasar Hofer <lebalz@outlook.com>
2022-10-17 23:34:20 +0200

    use await in material sync

--- material_sync.js
+++ material_sync.js
@@ -34,91 +34,101 @@ const ensureTrailingSlash = (path) => {
     return `${path}/`
 }
 
-Object.keys(CONFIG).forEach((klass) => {
-    const config = CONFIG[klass];
-    const gitignore = [];
-    const classDir = `versioned_docs/version-${klass}/`;
-    config.forEach((src) => {
-        var srcPath = undefined
-        var toPath = undefined
-        var ignore = [];
-        switch (typeof src) {
-            case 'string':
-                srcPath = src;
-                toPath = `${classDir}${relative2Doc(src)}`;
-                break;
-            case 'object':
-                srcPath = src.from;
-                if (src.to) {
-                    toPath = src.to
-                } else {
-                    toPath = `${classDir}${relative2Doc(src)}`
-                }
-                ignore = src.ignore;
-                break;
-        }
-        const isDir = fs.lstatSync(srcPath).isDirectory()
-        if (isDir) {
-            srcPath = ensureTrailingSlash(srcPath);
-        }
-        const parent = path.dirname(toPath);
-        if (!fs.existsSync(parent)) {
-            fs.mkdirSync(parent, {recursive: true})
-        }
+(async () => {
+    Object.keys(CONFIG).forEach(async (klass) => {
+        const config = CONFIG[klass];
+        const gitignore = [];
+        const classDir = `versioned_docs/version-${klass}/`;
+        const promises = []
+        config.forEach(async (src) => {
+            var srcPath = undefined
+            var toPath = undefined
+            var ignore = [];
+            switch (typeof src) {
+                case 'string':
+                    srcPath = src;
+                    toPath = `${classDir}${relative2Doc(src)}`;
+                    break;
+                case 'object':
+                    srcPath = src.from;
+                    if (src.to) {
+                        toPath = src.to
+                    } else {
+                        toPath = `${classDir}${relative2Doc(src)}`
+                    }
+                    ignore = src.ignore;
+                    break;
+            }
+            const isDir = fs.lstatSync(srcPath).isDirectory()
+            if (isDir) {
+                srcPath = ensureTrailingSlash(srcPath);
+            }
+            const parent = path.dirname(toPath);
+            if (!fs.existsSync(parent)) {
+                fs.mkdirSync(parent, { recursive: true })
+            }
 
-        if (isDir) {
-            const sanitizedClassDir = ensureTrailingSlash(toPath.replace(classDir, ''));
-            gitignore.push(`${sanitizedClassDir}*`)
-            const rsync = new Rsync()
-                .source(srcPath)
-                .destination(toPath)
-                .archive()
-                .delete();
-            if (ignore.length > 0) {
-                rsync.exclude(ignore)
-                gitignore.push(`!${sanitizedClassDir}${ignore}`)
-            }
-            rsync.exclude(['.sync.*', '*.nosync.*'])
-            rsync.execute((err, code, cmd) => {
-                if (!err) {
-                    console.log('✅', cmd)
-                } else {
-                    console.log('❌')
-                    console.log('   ', cmd)
-                    console.log('   ', err)
-                    console.log('   ', code)
-                    console.log('')
+            if (isDir) {
+                const sanitizedClassDir = ensureTrailingSlash(toPath.replace(classDir, ''));
+                gitignore.push(`${sanitizedClassDir}*`)
+                const rsync = new Rsync()
+                    .source(srcPath)
+                    .destination(toPath)
+                    .archive()
+                    .delete();
+                if (ignore.length > 0) {
+                    rsync.exclude(ignore)
+                    gitignore.push(`!${sanitizedClassDir}${ignore}`)
                 }
-            })
-        } else {
-            fs.copyFileSync(srcPath, toPath);
-            gitignore.push(toPath.replace(classDir, ''))
-        }
-        if (src.open) {
-            const folder = isDir ? toPath : parent;
-            try {
-                fs.mkdirSync(folder, {recursive: true})
-            } catch (e) {
-                console.log(e);
+                rsync.exclude(['.sync.*', '*.nosync.*'])
+                const rs = new Promise((resolve, reject) => {
+                    rsync.execute((err, code, cmd) => {
+                        if (!err) {
+                            console.log('✅', cmd)
+                            resolve()
+                        } else {
+                            console.log('❌')
+                            console.log('   ', cmd)
+                            console.log('   ', err)
+                            console.log('   ', code)
+                            console.log('')
+                            resolve();
+                        }
+                    });
+                })
+                promises.push(rs);
+            } else {
+                fs.copyFileSync(srcPath, toPath);
+                gitignore.push(toPath.replace(classDir, ''))
             }
-            const categoryPath = path.join(folder, '_category_.json');
-            gitignore.push(categoryPath.replace(classDir, ''));
-            let category = {
-                collapsible: true,
-                collapsed: false
-            };
-            if (fs.existsSync(categoryPath)) {
-                category = JSON.parse(fs.readFileSync(categoryPath));
-                category.collapsed = false;
-                category.collapsible = true;
-            }
-            fs.writeFileSync(
-                categoryPath, 
-                JSON.stringify(category, undefined, 2)
-            );
-           
-        }
-        fs.writeFileSync(`${classDir}.gitignore`, gitignore.join("\n"))
+            await Promise.all(promises);
+            if (src.open) {
+                const folder = isDir ? toPath : parent;
+                try {
+                    fs.mkdirSync(folder, { recursive: true })
+                } catch (e) {
+                    console.log(e);
+                }
+                const categoryPath = path.join(folder, '_category_.json');
+                console.log('---------- CAT', categoryPath)
+                gitignore.push(categoryPath.replace(classDir, ''));
+                let category = {
+                    collapsible: true,
+                    collapsed: false
+                };
+                if (fs.existsSync(categoryPath)) {
+                    category = JSON.parse(fs.readFileSync(categoryPath));
+                    category.collapsed = false;
+                    category.collapsible = true;
+                }
+                fs.writeFileSync(
+                    categoryPath,
+                    JSON.stringify(category, undefined, 2)
+                );
 
+            }
+            fs.writeFileSync(`${classDir}.gitignore`, gitignore.join("\n"))
+
+        })
     })
-})
+})();
