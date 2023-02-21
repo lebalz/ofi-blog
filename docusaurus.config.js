f3ee9934395e068626feff93e60987a292917ae0
Balthasar Hofer <lebalz@outlook.com>
2023-02-21 04:05:51 +0100

    try lunr search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -411,34 +411,12 @@ async function createConfig() {
           },
         };
       },
-      [
-        require.resolve("@cmfcmf/docusaurus-search-local"),
-        {
-          // Whether to also index the titles of the parent categories in the sidebar of a doc page.
-          // 0 disables this feature.
-          // 1 indexes the direct parent category in the sidebar of a doc page
-          // 2 indexes up to two nested parent categories of a doc page
-          // 3...
-          //
-          // Do _not_ use Infinity, the value must be a JSON-serializable integer.
-          indexDocSidebarParentCategories: 1,
-  
-  
-          // whether to index blog pages
-          indexBlog: false,
-  
-          // whether to index static pages
-          // /404.html is never indexed
-          indexPages: false,
-          // language of your documentation, see next section
-          language: "de",
-          // setting this to "none" will prevent the default CSS to be included. The default CSS
-          // comes from autocomplete-theme-classic, which you can read more about here:
-          // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
-          style: undefined,
-  
-        }
-      ]
+      [ require.resolve('docusaurus-lunr-search'), {
+        languages: ['en', 'de'], 
+        excludeRoutes: [
+            'docs/**/*', // exclude docs from indexing
+        ]
+      }],
     ],
     stylesheets: [
       {
