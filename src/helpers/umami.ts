9c183adeb6f4d0f2ea1b23ca306b8d8ee6ee1299
LeBalz <lebalz@outlook.com>
2023-05-01 16:12:43 +0200

    update umami

--- src/helpers/umami.ts
+++ src/helpers/umami.ts
@@ -1,7 +1,7 @@
 export const umamiReport = (event: string, value: Object) => {
     if (window && (window as any).umami) {
         try {
-            (window as any).umami.trackEvent(event, value);
+            (window as any).umami.track(event, value);
         } catch (e) {
             console.log('umami', e);
         }
