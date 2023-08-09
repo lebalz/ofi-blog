f21f61cb2ceaeb7da456ecba6beb11803209f0fa
LeBalz <lebalz@outlook.com>
2023-08-09 18:48:03 +0200

    fix msal in offline mode

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -10,79 +10,81 @@ import Head from "@docusaurus/Head";
 import siteConfig from '@generated/docusaurus.config';
 const { OFFLINE_MODE } = siteConfig.customFields as { OFFLINE_MODE?: boolean };
 
-const msalInstance = new PublicClientApplication(msalConfig);
-
+let msalInstance: PublicClientApplication;
+let Msal = ({ children }) => <>{children}</>;
 export const CommentContext = React.createContext(true);
 export const CommentProvider = CommentContext.Provider;
 
-const selectAccount = () => {
-  /**
-   * See here for more information on account retrieval:
-   * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
-   */
+if (!OFFLINE_MODE) {
+    msalInstance = new PublicClientApplication(msalConfig);
 
-  const currentAccounts = msalInstance.getAllAccounts();
-  if (!currentAccounts || currentAccounts.length < 1) {
-    return;
-  } else if (currentAccounts.length > 1) {
-    console.warn("Multiple accounts detected.");
-    rootStore.msalStore.setAccount(currentAccounts[0]);
-  } else if (currentAccounts.length === 1) {
-    rootStore.msalStore.setAccount(currentAccounts[0]);
-  }
-};
+    const selectAccount = () => {
+        /**
+         * See here for more information on account retrieval:
+         * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-common/docs/Accounts.md
+         */
 
-const handleResponse = (response) => {
-  /**
-   * To see the full list of response object properties, visit:
-   * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/request-response-object.md#response
-   */
-  rootStore.msalStore.setMsalInstance(msalInstance);
-  if (response !== null) {
-    rootStore.msalStore.setAccount(response.account);
-  } else {
-    selectAccount();
-  }
-};
+        const currentAccounts = msalInstance.getAllAccounts();
+        if (!currentAccounts || currentAccounts.length < 1) {
+            return;
+        } else if (currentAccounts.length > 1) {
+            console.warn("Multiple accounts detected.");
+            rootStore.msalStore.setAccount(currentAccounts[0]);
+        } else if (currentAccounts.length === 1) {
+            rootStore.msalStore.setAccount(currentAccounts[0]);
+        }
+    };
 
-msalInstance
-  .handleRedirectPromise()
-  .then(handleResponse)
-  .catch((error) => {
-    console.error(error);
-  });
+    const handleResponse = (response) => {
+        /**
+         * To see the full list of response object properties, visit:
+         * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/request-response-object.md#response
+         */
+        rootStore.msalStore.setMsalInstance(msalInstance);
+        if (response !== null) {
+            rootStore.msalStore.setAccount(response.account);
+        } else {
+            selectAccount();
+        }
+    };
 
-const Msal = observer(({ children }) => {
-  if (OFFLINE_MODE) {
-    return <>{children}</>;
-  }
-  return <MsalProvider instance={msalInstance}>{children}</MsalProvider>;
-});
+    msalInstance
+        .handleRedirectPromise()
+        .then(handleResponse)
+        .catch((error) => {
+            console.error(error);
+        });
+
+    Msal = observer(({ children }) => {
+        return <MsalProvider instance={msalInstance}>{children}</MsalProvider>;
+    });
+}
+
 
 // Default implementation, that you can customize
 function Root({ children }) {
-  const isBrowser = useIsBrowser();
-  if (isBrowser && !(window as any).store) {
-    (window as any).store = rootStore;
-  }
-  return (
-    <div>
-      <Head>
-        <meta property="og:description" content="Informatik Gymnasium Biel-Seeland" />
-        <meta
-          property="og:image"
-          content="https://ofi.gbsl.website/img/og-preview.png"
-        />
-      </Head>
-      <StoresProvider value={rootStore}>
-        <Msal>
-          <CommentProvider value={true}>            
-            {children}
-          </CommentProvider>
-        </Msal>
-      </StoresProvider>
-    </div>
-  );
+    const isBrowser = useIsBrowser();
+    if (isBrowser && !(window as any).store) {
+        (window as any).store = rootStore;
+    }
+    return (
+        <div>
+            <Head>
+                <meta property="og:description" content="Informatik Gymnasium Biel-Seeland" />
+                <meta
+                    property="og:image"
+                    content="https://ofi.gbsl.website/img/og-preview.png"
+                />
+            </Head>
+            <StoresProvider value={rootStore}>
+                <Msal>
+                    <CommentProvider value={true}>
+                        {children}
+                    </CommentProvider>
+                </Msal>
+            </StoresProvider>
+        </div>
+    );
 }
 
 export default Root;
