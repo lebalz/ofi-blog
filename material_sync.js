0cea69e14e4fe5a69b75f46fa9f3a9bcd8286497
LeBalz <lebalz@outlook.com>
2023-08-14 12:15:44 +0200

    keep template files

--- material_sync.js
+++ material_sync.js
@@ -18,6 +18,34 @@ const docBasePath = (src) => {
 
 /**
  * 
+ * @param {string} path
+ * @returns {string[]}
+ */
+const findMdTemplate = (src) => {
+    const mdFiles = [];
+    if (fs.lstatSync(src).isDirectory()) {
+        fs.readdirSync(src).forEach((file) => {
+            const fname = path.join(src, file);
+            if (fs.lstatSync(fname).isDirectory()) {
+                mdFiles.push(...findMdTemplate(fname));
+            } else if (file.endsWith('.md') || file.endsWith('.mdx')) {
+                if (file.startsWith('_')) {
+                    mdFiles.push(fname);
+                }
+            }
+        });
+    } else {
+        if (src.endsWith('.md') || src.endsWith('.mdx')) {
+            if (src.startsWith('_')) {
+                mdFiles.push(src);
+            }
+        }
+    }
+    return mdFiles;
+}
+
+/**
+ * 
  * @param {string} path 
  * @returns 
  */
@@ -40,6 +68,10 @@ if (process.env.WITHOUT_DOCS) {
     fs.renameSync('docs', '_docs')
     fs.mkdirSync('docs')
     fs.cpSync('_docs/home.md', 'docs/home.md')
+    /** copy all markdown-templates - otherwise some pages might fail */
+    findMdTemplate(path.join(__dirname, '_docs')).forEach((file) => {
+        fs.cpSync(file, file.replace('/_docs/', '/docs/'))
+    });
 }
 (async () => {
     Object.keys(CONFIG).forEach(async (klass) => {
